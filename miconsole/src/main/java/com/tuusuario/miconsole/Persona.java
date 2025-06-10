3067a55942ff990c36fdaed4111128b5058884ce
Marcelo Antonio <129465511+maofuentes@users.noreply.github.com>
2025-06-10 00:02:06 +0000

    Clase App y persona Configuradas

---
+++ miconsole/src/main/java/com/tuusuario/miconsole/Persona.java
@@ -0,0 +1,11 @@
+package com.tuusuario.miconsole;
+
+public class Persona {
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
